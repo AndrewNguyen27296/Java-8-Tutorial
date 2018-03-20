754f96203b181f5eb063d8e0403577dfb1aa7b6f
XinKaChu <pandarencute96@gmail.com>
2018-03-20 11:39:48 +0700

    Return Value With Lambda Expression

---
+++ LambdaExpWithReturnValue/src/infotech/com/ClientTest.java
@@ -0,0 +1,12 @@
+package infotech.com;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		MyInterface myInterface = (n1,n2) ->n1>n2;
+		
+		System.out.println(myInterface.test(40, 35));
+		System.out.println(myInterface.test(40, 45));
+	}
+
+}
---
+++ LambdaExpWithReturnValue/src/infotech/com/MyInterface.java
@@ -0,0 +1,6 @@
+package infotech.com;
+
+@FunctionalInterface
+public interface MyInterface {
+	boolean test (int n1, int n2);
+}
