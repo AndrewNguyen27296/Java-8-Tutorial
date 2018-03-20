b182dbeb243a7b3ad6d88930a3836c24873c1d25
XinKaChu <pandarencute96@gmail.com>
2018-03-20 15:04:20 +0700

    Reference to a contructor

---
+++ ReferenceToContructorInLamba/src/com/infotech/client/ClientTest.java
@@ -0,0 +1,25 @@
+package com.infotech.client;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class ClientTest {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(4,9,36,100);
+		numbers.forEach(System.out::println);
+		System.out.println("------------------------");
+		
+		List<Double> findSquareRoots = ClientTest.findSquareRoots(numbers, Integer::new);
+		findSquareRoots.forEach(System.out::println);
+		
+	}
+	
+	private static List<Double> findSquareRoots(List<Integer> numbers, Function<Integer, Integer> f){
+		List<Double> results = new ArrayList<>();
+		
+		numbers.forEach(x -> results.add(Math.sqrt(f.apply(x))));
+		return results;
+	}
+}
