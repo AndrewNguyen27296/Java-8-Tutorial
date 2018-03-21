cb64130c993007c7def312548fed958234c1d4fb
XinKaChu <pandarencute96@gmail.com>
2018-03-21 13:30:45 +0700

    Stream of Files

---
+++ StreamAPITest/src/com/infotech/StreamWithFileTest.java
@@ -0,0 +1,19 @@
+package com.infotech;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class StreamWithFileTest {
+	public static void main(String[] args) {
+		Path path = Paths.get("test.txt");
+		try (Stream<String> lines = Files.lines(path, Charset.forName("UTF-8"))){
+			lines.forEach(System.out::println);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
