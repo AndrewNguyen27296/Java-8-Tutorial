6221d6bea001f5578a72463da986b0979f1968aa
XinKaChu <pandarencute96@gmail.com>
2018-03-22 11:06:14 +0700

    date/time Duration  APIs

--- Java8DateTimeDemo/src/com/infotech/periodduration/PeriodAndDurationTest.java
+++ Java8DateTimeDemo/src/com/infotech/periodduration/PeriodAndDurationTest.java
@@ -1,13 +1,15 @@
 package com.infotech.periodduration;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 public class PeriodAndDurationTest {
 	public static void main(String[] args) {
 
-		// Create initialDate-now and finalDay
+		// Create initialDate-now and finalDate
 		LocalDate initialDate = LocalDate.now();
 		LocalDate finalDate = initialDate.plus(Period.ofDays(5));
 
@@ -18,5 +20,20 @@ public class PeriodAndDurationTest {
 		// Get day between now and 5 days later 2
 		long between = ChronoUnit.DAYS.between(initialDate, finalDate);
 		System.out.println(between);
+		
+		System.out.println("-------------------------------------");
+		
+		// Create initialTime-now and finalTime
+		LocalTime initialTime = LocalTime.of(6, 30,0);
+		LocalTime finalTime = initialTime.plus(Duration.ofSeconds(30));
+		
+		// Get time between now and 30 sec later
+		long seconds = Duration.between(initialTime, finalTime).getSeconds();
+		System.out.println(seconds);
+		
+		// Get time between now and 30 sec later 2
+		long between2  = ChronoUnit.SECONDS.between(initialTime, finalTime);
+		System.out.println(between2);
+		
 	}
 }
