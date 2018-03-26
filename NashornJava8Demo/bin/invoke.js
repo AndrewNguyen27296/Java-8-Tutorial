cd34973ef799523bca748eafd732f8d7420867d4
XinKaChu <pandarencute96@gmail.com>
2018-03-26 10:03:37 +0700

    Nashorn:Invoke JavaScript function

---
+++ NashornJava8Demo/bin/invoke.js
@@ -0,0 +1,7 @@
+/**
+ * Invoke js function
+ */
+var fun1 = function(name){
+	print('Hi, ' + name);
+	return "Greeting from Javascript";
+}
--- NashornJava8Demo/src/com/infotech/client/ClientTest.java
+++ NashornJava8Demo/src/com/infotech/client/ClientTest.java
@@ -3,19 +3,27 @@ package com.infotech.client;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class ClientTest {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, NoSuchMethodException {
 		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
 		ScriptEngine engine = scriptEngineManager.getEngineByName("nashorn");
 		
 		try {
-//			engine eval('Print HelloWorld!')
-			engine.eval(new FileReader("D:\\Documents\\IT Exercise\\Java Exercise\\Java 8 Tutorial\\NashornJava8Demo\\src\\demo.js"));
+//			read file js
+			engine.eval(new FileReader("D:\\Documents\\IT Exercise\\Java Exercise\\Java 8 Tutorial\\NashornJava8Demo\\src\\invoke.js"));
+			
+//			invoke js function fun1
+			Invocable invocable = (Invocable)engine;
+			
+			Object result = invocable.invokeFunction("fun1", "XINKACHU");
+			System.out.println(result);
+			
 		} catch (ScriptException e) {
 			e.printStackTrace();
 		}
